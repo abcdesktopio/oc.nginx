e5ebab1c0287e5f432f9e447efbb189dce496033
Alexandre DEVELY <koumoula@users.noreply.github.com>
2021-01-18 22:10:26 +0100

    export env JWT_DESKTOP_KEYS

--- composer/docker-entrypoint.sh
+++ composer/docker-entrypoint.sh
@@ -20,13 +20,13 @@ fi
 if [ -z "$JWT_DESKTOP_PAYLOAD_PRIVATE_KEY" ]
 then
    echo "JWT_DESKTOP_PAYLOAD_PRIVATE_KEY is not defined, settings to default value"
-   JWT_DESKTOP_PAYLOAD_PRIVATE_KEY="/config.payload/abcdesktop_jwt_desktop_payload_private_key.pem"
+   export JWT_DESKTOP_PAYLOAD_PRIVATE_KEY="/config.payload/abcdesktop_jwt_desktop_payload_private_key.pem"
 fi
 
 if [ -z "$JWT_DESKTOP_SIGNING_PUBLIC_KEY" ]
 then
    echo "JWT_DESKTOP_SIGNING_PUBLIC_KEY is not defined, settings to default value"
-   JWT_DESKTOP_SIGNING_PUBLIC_KEY="/config.signing/abcdesktop_jwt_desktop_signing_public_key.pem"
+   export JWT_DESKTOP_SIGNING_PUBLIC_KEY="/config.signing/abcdesktop_jwt_desktop_signing_public_key.pem"
 fi
 
 if [ ! -d "/config.signing" ]; then
