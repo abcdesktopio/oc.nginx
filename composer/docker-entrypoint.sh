f77f26006a0cbe9577bb40740ee05547cd0cea5b
Alexandre DEVELY <12896316+alexandredevely@users.noreply.github.com>
2023-04-18 21:49:35 +0200

    Update docker-entrypoint.sh

--- composer/docker-entrypoint.sh
+++ composer/docker-entrypoint.sh
@@ -13,9 +13,10 @@ else
         # replace pyos 		by FQDN pyos.default.svc.cluster.local
 	# sed -i "s/127.0.0.11/kube-dns.kube-system.svc.cluster.local./g" 	/etc/nginx/sites-enabled/default 
 	RESOLVER=$(grep -m 1 nameserver /etc/resolv.conf | awk '{ print $2 }')
+	echo Resolver for nginx is $RESOLVER
 	cp /etc/nginx/sites-enabled/default /tmp/default
         sed -i "s/127.0.0.11/$RESOLVER/g" /tmp/default
-	sed -i "s/pyos/pyos.abcdesktop.svc.cluster.local./g" /tmp/default
+	# sed -i "s/pyos/pyos.abcdesktop.svc.cluster.local./g" /tmp/default
 	echo New site configuration
 	cp  /tmp/default /etc/nginx/sites-enabled/default
 	cat /etc/nginx/sites-enabled/default
